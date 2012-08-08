179ff226f8839a5c6dbb456a64a9046849413d1d
Renne Rocha <me@rennerocha.com>
2012-08-08 08:50:38 -0300

    Fixing identation to return CSV correctly

--- lhc_cash/__init__.py
+++ lhc_cash/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, request
 app = Flask(__name__)
 
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/balance/<int:year>/<int:month>/')
 def balance(year=None, month=None):
 
-	format = request.args.get('format', 'json')
+    format = request.args.get('format', 'json')
 
     balance = {
         'expected': 600,
@@ -19,7 +19,7 @@ def balance(year=None, month=None):
         balance['month'] = month
 
     if format == 'csv':
-    	data = "{0},{1},{2},{3}".format(balance['expected'], balance['actual'], balance['year'], balance['month'])
-    	return Response(data, mimetype='text/csv')
+        data = "{0},{1},{2},{3}".format(balance['expected'], balance['actual'], balance['year'], balance['month'])
+        return Response(data, mimetype='text/csv')
 
     return jsonify(**balance)
