e436820c80b09ef63f5448da7950940cd12f7bc8
Renne Rocha <me@rennerocha.com>
2012-08-08 09:01:51 -0300

    Returning balance of the current month when not indicated.

--- lhc_cash/__init__.py
+++ lhc_cash/__init__.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
+import datetime
+
 from flask import Flask, jsonify, Response, request
 app = Flask(__name__)
 
 
 @app.route('/balance/', methods=['GET', ])
 @app.route('/balance/<int:year>/<int:month>/')
-def balance(year=None, month=None):
+def return_balance(year=None, month=None):
+
+    if not year or not month:
+        year = datetime.date.today().year
+        month = datetime.date.today().month
 
     format = request.args.get('format', 'json')
 
     balance = {
         'expected': 600,
         'actual': -600,
+        'year': year,
+        'month': month,
     }
-    if year:
-        balance['year'] = year
-    if month:
-        balance['month'] = month
 
     if format == 'csv':
         data = "{0},{1},{2},{3}".format(balance['expected'], balance['actual'], balance['year'], balance['month'])
