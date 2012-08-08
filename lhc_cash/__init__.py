4254c240f8030b43dbb6bdd74a788bf477ee5933
Renne Rocha <me@rennerocha.com>
2012-08-07 22:21:34 -0300

    Accepting year and month parameters.

--- lhc_cash/__init__.py
+++ lhc_cash/__init__.py
@@ -4,10 +4,16 @@ app = Flask(__name__)
 
 
 @app.route('/balance/', methods=['GET', ])
-def balance():
+@app.route('/balance/<int:year>/<int:month>/')
+def balance(year=None, month=None):
     balance = {
         'expected': 600,
         'actual': -600,
     }
+    if year:
+        balance['year'] = year
+    if month:
+        balance['month'] = month
+
     return jsonify(**balance)
 
