33a3269644ed164e5c9bdda08bb96b030bf21933
Renne Rocha <me@rennerocha.com>
2012-08-08 08:04:57 -0300

    Returning CSV if asked

--- lhc_cash/__init__.py
+++ lhc_cash/__init__.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 app = Flask(__name__)
 
 
 @app.route('/balance/', methods=['GET', ])
 @app.route('/balance/<int:year>/<int:month>/')
 def balance(year=None, month=None):
+
+	format = request.args.get('format', 'json')
+
     balance = {
         'expected': 600,
         'actual': -600,
@@ -15,5 +18,8 @@ def balance(year=None, month=None):
     if month:
         balance['month'] = month
 
-    return jsonify(**balance)
+    if format == 'csv':
+    	data = "{0},{1},{2},{3}".format(balance['expected'], balance['actual'], balance['year'], balance['month'])
+    	return Response(data, mimetype='text/csv')
 
+    return jsonify(**balance)
